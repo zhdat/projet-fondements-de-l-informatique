e30fa618b05e2c56c217f57e14945d1623aa1d5f
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-01-05 13:40:48 +0100

    create_output_image

--- Projet-Base-Algo/src/fusion_iterative.c
+++ Projet-Base-Algo/src/fusion_iterative.c
@@ -28,6 +28,24 @@ void perform_merge(rag r, double seuil){ /* effectue itérativement des fusions 
 }
 
 image create_output_image(rag r){ /* crée une image où chaque block est affiché avec la couleur moyenne de son block parent. */
-    image output_image;
-    return output_image;
+    int i;
+    int j;
+    image img = r->img;
+    image img_out;
+    int dim = image_give_dim(img);
+    int L = image_give_largeur(img);
+    int H = image_give_hauteur(img);
+    int mean_color[3];
+    int n_block;
+    image_initialize(img_out, L, H, dim);
+    for (i = 0; i < H; i++)
+    {
+        for (j = 0; j < L; j++)
+        {
+            n_block = (i*L) + j;
+            RAG_give_mean_color(r, n_block, mean_color);
+            image_write_pixel(img_out, i, j, (unsigned char) mean_color);
+        }
+    }
+    return img_out;
 }
