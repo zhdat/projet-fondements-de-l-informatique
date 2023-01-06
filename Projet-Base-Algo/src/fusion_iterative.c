3f0d8ad8712d0e7bf301b0a64f25172a7256c260
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-01-06 13:45:20 +0100

    seuil

--- Projet-Base-Algo/src/fusion_iterative.c
+++ Projet-Base-Algo/src/fusion_iterative.c
@@ -25,7 +25,7 @@ void perform_merge(rag r, double seuil){ /* effectue itérativement des fusions 
     double tmp;
 
     erreur_init = r->erreur_partition;
-    erreur_seuil = erreur_init * seuil/100;
+    erreur_seuil = erreur_init * seuil/100.000;
     erreur_after_merge = erreur_init;
     tmp = RAG_give_closest_region(r, &i, &j);
 
